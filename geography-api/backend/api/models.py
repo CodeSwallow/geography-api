eba79f6d04b11ca1d97233c5374cd3ba6f637619
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-03-08 16:16:52 -0600

    Added USState model and Pillow dependancy

--- geography-api/backend/api/models.py
+++ geography-api/backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class USState(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    capital = models.CharField(max_length=255, null=True, blank=True)
+    nickname = models.CharField(max_length=255, null=True, blank=True)
+    abbreviation = models.CharField(max_length=2, null=True, blank=True)
+    population = models.PositiveIntegerField(default=True)
+    flag_image = models.ImageField(default=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.nickname}"
